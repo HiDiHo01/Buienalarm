328f319b486f90d7539b94aafee162e8ed542191
Roy <96914811+HiDiHo01@users.noreply.github.com>
2025-06-27 15:29:20 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
 import sys
 import os
+from pathlib import Path
+repo_root = Path(__file__).parent.parent
+sys.path.insert(0, str(repo_root))
 # Voeg de projectroot toe aan sys.path zodat 'custom_components' geïmporteerd kan worden
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
