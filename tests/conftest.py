61acc80ddebebfaf45df52150e5afbbc0fad8845
Roy <96914811+HiDiHo01@users.noreply.github.com>
2025-06-27 13:51:54 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,7 @@
+import sys
+import os
+# Voeg de projectroot toe aan sys.path zodat 'custom_components' geïmporteerd kan worden
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import asyncio
 import pytest
 import pytest_asyncio
