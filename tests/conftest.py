781787db405b6775f2892f763aac108890f7b613
Roy <96914811+HiDiHo01@users.noreply.github.com>
2025-06-27 15:44:25 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,16 @@
 import sys
 import os
 from pathlib import Path
+
+import pytest
+
 repo_root = Path(__file__).parent.parent
 sys.path.insert(0, str(repo_root))
-# Voeg de projectroot toe aan sys.path zodat 'custom_components' geïmporteerd kan worden
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+pytest_plugins = ["pytest_homeassistant_custom_component"]
+
+from homeassistant.const import Platform
+
+@pytest.fixture(autouse=True)
+def auto_enable_custom_integrations(enable_custom_integrations):
+    """Enable custom integrations automatically for all tests."""
