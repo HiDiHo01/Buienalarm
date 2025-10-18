5f45582182742902da6f5d44039bcf8a3b6d41ff
Roy <hidiho@gmail.com>
2025-10-18 16:16:41 +0200

    Updated version to 2025.10.18

--- custom_components/buienalarm/const.py
+++ custom_components/buienalarm/const.py
@@ -18,7 +18,7 @@ DATA_KEY: Final[str] = "data"
 # Integration metadata
 NAME: Final[str] = "Buienalarm"
 DOMAIN: Final[str] = "buienalarm"
-VERSION: Final[str] = "2025.9.17"
+VERSION: Final[str] = "2025.10.18"
 ATTR_ATTRIBUTION: Final[str] = "Data provided by Buienalarm"
 
 # Default configuration
